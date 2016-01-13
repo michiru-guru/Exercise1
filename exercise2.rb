c167d807de5454b0b6cb218bbd2eadd886115e09
Michael Morley <MichaelMorley@Michaels-MacBook-Pro.local>
2016-01-12 23:14:30 -0500

    true input

--- exercise2.rb
+++ exercise2.rb
@@ -2,3 +2,5 @@
 puts "dinner tip #{ 55.0 * 0.2} " + "numbers " + 9.to_s
 # statistics of trout fish population increase
 puts "The population of trout fish multiplying in 2014-2015 = #{ 45628 * 7839 }"
+#true value
+puts (10 < 20 && 30 < 20) || !(10 == 11)
