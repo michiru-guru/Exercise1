4b63b30b9e500cba83d5ad6aae7f9489b3d8c877
Michael Morley <MichaelMorley@Michaels-MacBook-Pro.local>
2016-01-12 23:09:37 -0500

    trout fish population

--- exercise2.rb
+++ exercise2.rb
@@ -1,2 +1,4 @@
 #This is the answer to calculate a good tip
 puts "dinner tip #{ 55.0 * 0.2} " + "numbers " + 9.to_s
+# statistics of trout fish population increase
+puts "The population of trout fish multiplying in 2014-2015 = #{ 45628 * 7839 }"
