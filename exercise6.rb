d1df68e0ac2de482cba51e8d2c8462eaef420fdc
Michael Morley <MichaelMorley@Michaels-MacBook-Pro.local>
2016-01-13 18:59:25 -0500

    list checker

--- exercise6.rb
+++ exercise6.rb
@@ -5,3 +5,16 @@ grocery_list.each do |item|
   puts "* #{item}"
 end
 puts grocery_list.length
+
+puts grocery_list.include?("bananna")
+if "true"
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas today"
+end
+
+#   puts "true"
+# end
+# else
+#   puts "false"
+# end
