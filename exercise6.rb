ce7d912e2e1dc6144c0152a04c7a1f739d54901b
Michael Morley <MichaelMorley@Michaels-MacBook-Pro.local>
2016-01-13 18:27:10 -0500

    number of items

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,7 @@
 grocery_list = ["carrots", "tooth_paste", "toilet_paper", "cucumber", "lasagne", "coconut"]
   puts grocery_list
+  grocery_list << "bread"
 grocery_list.each do |item|
   puts "* #{item}"
 end
+puts grocery_list.length
